ead42c8d778e41d8a04bac31f367d1f4e73b55f1
kalisma84@gmail.com <kalisma84@gmail.com@112c6201-783e-ceb7-77f6-b340fbc37538>
2013-03-04 08:24:40 +0000

    고협 : 마이페이지 index 끝.

---
+++ trunk/safecoms/src/com/safecoms/web/mypage/controller/MyPage.java
@@ -0,0 +1,15 @@
+package com.safecoms.web.mypage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/safecoms/mypage")
+public class MyPage {
+	
+	@RequestMapping(value="")
+	public String index() {
+		return "/safecoms/mypage/index";
+	}
+
+}
